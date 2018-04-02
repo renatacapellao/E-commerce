370106360cb6df8055031b02e4f99cf83510a0cc
Matheus Jordan <matheus_jordan@yahoo.com.br>
2018-04-02 08:51:38 -0300

    Atualização PhP

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!-- Footer -->
+	<section id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
+					</ul>
+				</div>
+				</hr>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					<p></p>
+					<p class="h6">&copy Todos os direitos reservados.<a class="text-green ml-2" href="#" target="_blank">Nome da empresa</a></p>
+				</div>
+				</hr>
+			</div>
+		</div>
+	</section>
+	<!-- ./Footer -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>E-commerce Eletronicos</title>
+
+    <!-- Bootstrap -->
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Estilo Personalizado -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/estilo.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+  		<div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        	<div class="navbar-header">
+          		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+          		</button>
+          		<a class="navbar-brand" href="#">Home</a>
+        	</div>
+
+        	<!-- Collect the nav links, forms, and other content for toggling -->
+        	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          		<ul class="nav navbar-nav">
+            		<li><a href="sobre.html">Sobre</a></li>
+              		<li><a href="#">Fale conosco</a></li>
+            		<li class="dropdown">
+              			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Produtos<span class="caret"></span></a>
+              			<ul class="dropdown-menu">
+			                <li><a href="#">Categorias</a></li>
+			                <li><a href="#">Marcas</a></li>
+			                <li role="separator" class="divider"></li>
+              				<li><a href="#">Destaques</a></li>
+              				<li><a href="#">Promoções</a></li>
+              			</ul>
+            		</li>
+          		</ul>
+          		<form class="navbar-form navbar-left">
+            		<div class="form-group">
+              			<input type="text" class="form-control" placeholder="Search">
+            		</div>
+            		<button type="button" class="btn btn-default">
+      					<span class="glyphicon glyphicon-search"></span>
+    				</button>
+          		</form>
+          		<ul class="nav navbar-nav navbar-right">
+            		<li><a href="#"><i class="glyphicon glyphicon-shopping-cart"></i></a></li>
+            		<li class="dropdown">
+              			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-user"></i><span class="caret"></span></a>
+              			<ul class="dropdown-menu">
+                			<li><a href="#">Minha conta</a></li>
+			                <li><a href="#">Histórico</a></li>
+			                <li><a href="#">Meus pedidos</a></li>
+			                <li role="separator" class="divider"></li>
+			                <li><a href="#">Log out</a></li>
+			            </ul>
+            		</li>
+          		</ul>
+        	</div><!-- /.navbar-collapse -->
+      	</div><!-- /.container-fluid -->
+    </nav>
