e56c90785fd32c5f2edceebd550a358d520824c9
Matheus Jordan <matheus_jordan@yahoo.com.br>
2018-04-06 18:55:04 -0300

    linkagem

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
         	<!-- Collect the nav links, forms, and other content for toggling -->
         	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           		<ul class="nav navbar-nav">
-            		<li><a href="sobre.html">Sobre</a></li>
+            		<li><a href="sobre.php">Sobre</a></li>
               		<li><a href="#">Fale conosco</a></li>
             		<li class="dropdown">
               			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Produtos<span class="caret"></span></a>
