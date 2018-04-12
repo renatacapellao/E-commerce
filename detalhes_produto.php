f0badc8cc6abc91a1f4436610218b9694822ab22
Matheus Jordan <matheus_jordan@yahoo.com.br>
2018-04-11 22:36:48 -0300

    Jordan commit

--- detalhes_produto.php
+++ detalhes_produto.php
@@ -9,12 +9,12 @@
 	$categoria = NULL;
 	$uso = NULL;
 
-    if(!isset($_GET["ID_PRODUTO"])) {
+    if(!isset($_GET["id"])) {
       echo "<script>document.location.href = 'index.php' </script>";
     } else {
         $cn = mysqli_connect("localhost", "root", "", "e-comerce");
 
-        $id = mysqli_real_escape_string($cn, $_GET["ID_PRODUTO"]);
+        $id = mysqli_real_escape_string($cn, $_GET["id"]);
 
         $q = mysqli_query($cn, "SELECT * FROM PRODUTO WHERE ID_PRODUTO = $id");
 
@@ -23,7 +23,6 @@
           $descricao = $r["DESCRICAO"];
           $preco = $r["PRECO"];
           $imagem = $r["IMAGEM"];
-		  $categoria = $r["CATEGORIA"];
 		  $uso = $r["USO"];
         } else {
           echo "<script>document.location.href = 'index.php;' </script>";
@@ -38,18 +37,19 @@
 	<div class ="container">
 		<div class = "col-md-4 col-sm-4 col-xs-12 produtodesc_img">
 			<img src="img/<?=$imagem; ?>" style="height:220px;width:100%;margin-top:80px;margin-bottom:40px;">
+			<h3 style="text-align:center;font-weight:normal;"><?=$preco?></h3>
 			<a href="#" class="btn btn-primary" style="margin-left:25%;width:50%;">Add ao carrinho</a>
 		</div>
 		
 		<div class = "col-md-6 col-sm-6 col-xs-12 produtodesc_texto">
-			<h3><?=$titulo; ?></h3>
+			<h3><?=$nome; ?></h3>
 			<p><?=$descricao ?></p>
-      <small><?=$preco?></small>
+      
 		</div>
 	</div>
 	
 
-  <?php require_once("../footer.php"); ?>
+  <?php require_once("footer.php"); ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
