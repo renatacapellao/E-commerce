c2bc162dd0399de2851df0395219a1c27790bb72
Matheus Jordan <matheus_jordan@yahoo.com.br>
2018-04-14 13:21:41 -0300

    adicionei algo

--- busca.php
+++ busca.php
@@ -5,7 +5,7 @@ $cn = mysqli_connect("localhost", "root", "", "e-comerce");
 
 $busca = $_POST['palavra'];// palavra que o usuario digitou
 
-$busca_query = mysqli_query($cn,"SELECT * FROM PRODUTO WHERE NOME OR DESCRICAO LIKE '%$busca%'");//faz a busca com as palavras enviadas
+$busca_query = mysqli_query($cn,"SELECT * FROM PRODUTO WHERE NOME LIKE '%$busca%'");//faz a busca com as palavras enviadas
 
 if (empty($busca_query)) { //Se nao achar nada, lança essa mensagem
     echo "Nenhum registro encontrado.";
